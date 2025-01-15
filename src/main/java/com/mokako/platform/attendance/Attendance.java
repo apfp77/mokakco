4835bfef50c9f589da0fcceb512c30ba1adf7b25
apfp77 <apfp77@naver.com>
2025-01-15 14:01:44 +0900

    feat: 출석부 entity 추가

--- src/main/java/com/mokako/platform/attendance/Attendance.java
+++ src/main/java/com/mokako/platform/attendance/Attendance.java
@@ -1,13 +1,11 @@
 package com.mokako.platform.attendance;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 
 @Entity
@@ -20,7 +18,48 @@ public class Attendance {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private Long discord_id;
+    @Column
+    private Long userId;
 
-    private LocalDateTime attendanceTime;
+    @Column(nullable = false)
+    private LocalDateTime entryTime;
+
+    @Column
+    private LocalDateTime exitTime;
+
+    @Column
+    private Integer durationMinutes; // 출입 시간과 퇴실 시간 차이 (분 단위)
+
+    protected Attendance(Long userId) {
+        this.userId = userId;
+    }
+
+    protected Attendance(Long userId, LocalDateTime entryTime){
+        this.userId = userId;
+        this.entryTime = entryTime;
+    }
+
+    // entryTime이 작성될때 시간차이 계산
+    @PreUpdate
+    public void calculateDuration() {
+        if (entryTime != null && exitTime != null) {
+            this.durationMinutes = (int) Duration.between(entryTime, exitTime).toMinutes();
+        }
+    }
+
+    protected void entry(){
+        this.entryTime = LocalDateTime.now();
+    }
+
+    protected void exit(){
+        this.exitTime = LocalDateTime.now();
+    }
+
+    // 하루를 넘어가는 경우 당일 퇴근시간을 23시 59분 59초로 설정
+    protected void lastRecordedExit(){
+        this.exitTime = this.entryTime
+                .withHour(23)
+                .withMinute(59)
+                .withSecond(59);
+    }
 }
