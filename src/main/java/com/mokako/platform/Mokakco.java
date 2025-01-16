725c61bb935be2848768d77b37293972d61d9107
apfp77 <apfp77@naver.com>
2025-01-17 03:07:05 +0900

    refactor: 서비스 이름 변경

---
+++ src/main/java/com/mokako/platform/Mokakco.java
@@ -0,0 +1,13 @@
+package com.mokako.platform;
+
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mokakco extends ListenerAdapter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mokakco.class, args);
+    }
+}
