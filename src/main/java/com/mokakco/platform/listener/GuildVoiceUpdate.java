c7f647e500ffb6daa1b4c531ce78a1a81bb55723
apfp77 <apfp77@naver.com>
2025-01-31 16:36:43 +0900

    refactor: 시간에 따른 알람 서비스 변경

--- src/main/java/com/mokakco/platform/listener/GuildVoiceUpdate.java
+++ src/main/java/com/mokakco/platform/listener/GuildVoiceUpdate.java
@@ -1,19 +1,19 @@
 package com.mokakco.platform.listener;
 
+import com.mokakco.platform.attendance.AttendanceNotification;
 import com.mokakco.platform.attendance.AttendanceService;
-import com.mokakco.platform.configure.DiscordConfigure;
+import com.mokakco.platform.attendance.TimeSession;
 import com.mokakco.platform.member.UserService;
 import net.dv8tion.jda.api.entities.Member;
-import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.entities.channel.middleman.AudioChannel;
 import net.dv8tion.jda.api.events.guild.voice.GuildVoiceUpdateEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
+import java.time.Clock;
 import java.time.LocalDateTime;
 import java.util.Objects;
 
@@ -30,12 +30,14 @@ public class GuildVoiceUpdate extends ListenerAdapter {
 
     private final UserService userService;
     private final AttendanceService attendanceService;
-    private final DiscordConfigure discordConfigure;
+    private final AttendanceNotification notifyChannel;
+    private final Clock clock;
 
-    public GuildVoiceUpdate(UserService userService, AttendanceService attendanceService, @Lazy DiscordConfigure discordConfigure) {
+    public GuildVoiceUpdate(UserService userService, AttendanceService attendanceService, AttendanceNotification notifyChannel, Clock clock) {
         this.userService = userService;
         this.attendanceService = attendanceService;
-        this.discordConfigure = discordConfigure;
+        this.notifyChannel = notifyChannel;
+        this.clock = clock;
     }
 
 
@@ -56,47 +58,16 @@ public class GuildVoiceUpdate extends ListenerAdapter {
             }
             Long userId = userService.findUserIdByDiscordId(member.getIdLong());
             attendanceService.entry(userId);
-            TextChannel channel = discordConfigure.getChannel(attendanceNotificationChannelId);
-                /**
-                 * 오전(08시 ~ 12시)반, 오후(1시 ~ 6시)반, 저녁(8시 ~ 12시)반
-                 * 각 시간때마다 한 시간 이상이면 출석을 완료했다는 메시지를 보냄
-                 * 00시 ~ 08시 사이에는 알람을 보내지 않는다
-                 */
-                // 오전반
-                if (LocalDateTime.now().getHour() >= 8 && LocalDateTime.now().getHour() < 12) {
-                    if (attendanceService.findTodayAttendance(userId,
-                            LocalDateTime.now().withHour(7)
-                                    .withMinute(59)
-                                    .withSecond(59), LocalDateTime.now().withHour(11)
-                                    .withMinute(59)
-                                    .withSecond(59))) {
-                        channel.sendMessage(String.format("%s 님이 오전반에 출석하였습니다", member.getEffectiveName())).queue();
-                    }
-                }
-                // 오후반
-                if (LocalDateTime.now().getHour() >= 13 && LocalDateTime.now().getHour() < 18) {
-                    if (attendanceService.findTodayAttendance(userId,
-                            LocalDateTime.now().withHour(12)
-                                    .withMinute(59)
-                                    .withSecond(59), LocalDateTime.now().withHour(17)
-                                    .withMinute(59)
-                                    .withSecond(59))) {
-                        channel.sendMessage(String.format("%s 님이 오후반에 출석하였습니다", member.getEffectiveName())).queue();
-                    }
-                }
-                // 저녁반
-                if (LocalDateTime.now().getHour() >= 20) {
-                    if (attendanceService.findTodayAttendance(userId,
-                            LocalDateTime.now().withHour(19)
-                                    .withMinute(59)
-                                    .withSecond(59), LocalDateTime.now().withHour(23)
-                                    .withMinute(59)
-                                    .withSecond(59))) {
-                        channel.sendMessage(String.format("%s 님이 저녁반에 출석하였습니다", member.getEffectiveName())).queue();
-                    }
-                }
+            
+            notifyChannel
+                    .sendNotification(
+                            member.getIdLong(),
+                            attendanceNotificationChannelId,
+                            String.format("%s님이 %s에 출석하였습니다.",
+                                    member.getEffectiveName(), TimeSession.findSessionByTime(LocalDateTime.now(clock)))
+                    );
             botLogger.info("{} + '님이' + {} + '채널에 참여했습니다.'", member.getEffectiveName(), joinedChannel.getName());
-        } else if (leftChannel != null) {
+        } else {
         // 채널 나감
             Long userId = userService.findUserIdByDiscordId(member.getIdLong());
             attendanceService.exit(userId);
