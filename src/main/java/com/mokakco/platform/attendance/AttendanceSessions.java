d17b3d90d5928a57efce9ff17bbddb062bccd22f
apfp77 <apfp77@naver.com>
2025-02-04 16:57:29 +0900

    feat: AttendanceSessions 날짜 추가

--- src/main/java/com/mokakco/platform/attendance/AttendanceSessions.java
+++ src/main/java/com/mokakco/platform/attendance/AttendanceSessions.java
@@ -1,6 +1,8 @@
 package com.mokakco.platform.attendance;
 
 import jakarta.persistence.*;
+
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Entity
@@ -18,12 +20,21 @@ public class AttendanceSessions {
     private Long userId;
 
     @Column
+    private LocalDate date;
+
+    @Column
     private Integer stayDurationMinutes;
 
     protected AttendanceSessions() {}
 
-    protected AttendanceSessions(Long userId, LocalDateTime entryTime) {
+    protected AttendanceSessions(Long userId, LocalDateTime entryTime, Integer stayDurationMinutes, LocalDate date) {
         this.userId = userId;
         this.timeSession = new TimeSession(entryTime);
+        this.date = date;
+        this.stayDurationMinutes = stayDurationMinutes;
+    }
+
+    protected void updateStayDurationMinutes(Integer stayDurationMinutes) {
+        this.stayDurationMinutes = stayDurationMinutes;
     }
 }
